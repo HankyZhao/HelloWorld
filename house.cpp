4b01ee9644a984284c698406f90be78e12ec8b51
HankyZhao <hankyz@HankyZhaodeMacBook-Pro.local>
2018-01-20 21:32:52 +0800

    didn't really do anything

--- house.cpp
+++ house.cpp
@@ -4,5 +4,12 @@ int main () {
 	int a = 0;
 	int b = 1;
 	cout << a + b << endl;
+	if (a) {
+		//don't do anything
+	}
+	else {
+		//nothing would happened
+	}
+	//now everything looks good
 	return 0;
 }
